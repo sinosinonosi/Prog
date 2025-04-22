6de15e3426f7b928d545b25f01fa19c231994409
sinosinonosi <enricpru@campus.monlau.com>
2025-04-22 12:42:55 +0200

    Delete demo-cq directory

--- demo-cq/src/main/java/com/example/eprunonosa/demo_ep/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cquintas.demo_cq.repository;
-
-import com.example.cquintas.demo_cq.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- demo-cq/src/main/java/com/example/eprunonosa/demo_ep/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cquintas.demo_cq.repository;
-
-import com.example.cquintas.demo_cq.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
